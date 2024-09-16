f029013060c4561d7c25bcb471c1050b1d002a72
BienBienBuen <33363816+BienBienBuen@users.noreply.github.com>
2024-09-16 23:38:23 +0800

    push

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import yfinance as yf
 from iexfinance.stocks import Stock
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     print("Hello, World!")
 
 #we can try a IEX Cloud API for financial data
+    data = np.zeros(2)
+    df = pd.DataFrame(data)
+    print(df)
