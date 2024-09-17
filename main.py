06b64423bf229c10035e3ecace72d74a4a925f71
BienBienBuen <33363816+BienBienBuen@users.noreply.github.com>
2024-09-17 22:00:22 +0800

    push

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from iexfinance.stocks import Stock
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import nasdaqdatalink as ndl
+import datetime
+
+"""using nasdaqdatalink"""
+def data_to_csv(ticker, start, end):
+    ndl.ApiConfig.api_key = "nsrdu2JfgqSVB4Czoca6"
+    table = ndl.get_table('QDL/BITFINEX', date={'gte': start, 'lte': end} , code=ticker)
+    print(type(table))
+    print(table)
+
+
 
 if __name__ == "__main__":
     print("Hello, World!")
+    data_to_csv('BTCUSD', datetime.datetime(2019, 1, 1), datetime.datetime(2020, 1, 31))
 
-#we can try a IEX Cloud API for financial data
-    data = np.zeros(2)
-    df = pd.DataFrame(data)
-    print(df)
+
