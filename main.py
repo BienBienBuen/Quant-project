390358b1b91a1c4c8a86e26aa84b804d30f0a9eb
KrishiSS <seksaria.krishi@gmail.com>
2024-09-18 15:54:48 +0100

    1

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ import datetime
 #     print(table)
 
 """using yfinance"""
-def data_to_csv(ticker, start, end):
-    data = yf.download(ticker, start=start, end=end)
-    print(data)
+def download_data(stock, start, end):
+    stock_data = {}
+    ticker = yf.download(stock, start,end, progress=False)
+    stock_data['price'] = ticker['Adj Close']
+    return pd.DataFrame(stock_data)
+
+start_date = datetime.datetime(2013,1,1)
+end_date = datetime.datetime(2024,6,1)
+slope = 1.1153621020607556
+intercept = -0.6439426102796257
 
 if __name__ == "__main__":
-    print("Hello, World!")
-    data_to_csv('BTCUSD', datetime.datetime(2019, 1, 1), datetime.datetime(2020, 1, 31))
-
+    SGD = download_data('SGD=X', start_date, end_date)
+    EUR = download_data('EUR=X', start_date, end_date)
+    residues = EUR - slope * SGD - intercept 
+    plt.plot(residues)
+    plt.show()
 
