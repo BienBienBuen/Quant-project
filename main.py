c443c93ba8eecad717e8470b515e52c612f54672
BienBienBuen <33363816+BienBienBuen@users.noreply.github.com>
2024-09-20 01:23:19 +0800

    push!

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from utils import download_data, filter_non_null_rows, perform_regression
 
 start_date = datetime.datetime(2012,12,1)
 end_date = datetime.datetime(2012,12,31)
-slope = 0.9131566687899764 
-intercept = 0.22382541828609814
+slope = 0.8350097957816879
+intercept = 0.31876376509492976
 invested = 0
 
 """backtest"""
@@ -20,6 +20,8 @@ def backtest(start_date:datetime.datetime, end_date:datetime.datetime, ticker_li
     df_list = [download_data(ticker, start_date, end_date) for ticker in ticker_list]
     filtered_df = filter_non_null_rows(df_list, ticker_list)
     result_df = strat(data=filtered_df, initial_capital=initial_capital)
+
+    """for calculations of how well the strategy performed"""
 
     capital_over_time = result_df['capital'].values
     daily_returns = np.diff(capital_over_time) / capital_over_time[:-1]
@@ -38,8 +40,9 @@ def backtest(start_date:datetime.datetime, end_date:datetime.datetime, ticker_li
     return result_df, capital_over_time, sharpe_ratio, max_drawdown
 
 
+
 """strategies"""
-def inversion_strategy(data:pd.DataFrame, initial_capital:str, ratio=0.5, inspection_window=300):
+def inversion_strategy(data:pd.DataFrame, initial_capital:str, ratio=slope, inspection_window=360):
 
 
     column_names = data.columns.tolist()
@@ -58,22 +61,55 @@ def inversion_strategy(data:pd.DataFrame, initial_capital:str, ratio=0.5, inspec
         # Save the residual to the DataFrame
         data.at[data.index[i], 'residual'] = residual_new
     
+    #data['spread'] = data['residual'].rolling(window=100).std()
     data['signal'] = data['residual'].apply(reversion_signal)
+    data['capital'] = initial_capital
+    data['long'] = 0
+    data['short'] = 0
+    data['cash'] = initial_capital
 
-    # for i in range(inspection_window, len(data)):
-    #     if data['signal'][i] == 1 and data['signal'][i-1] == 0:
-    #         #enter long position
-    #         portfolio_weight = [ratio, 1-ratio]
-    #         weight_dict = {column_names[i]: portfolio_weight[i] for i in range(len(column_names))}
-    #         shares = {ticker: (initial_capital * weight)/data[ticker][i] for ticker, weight in weight_dict.items()}
-    #         data['capital'][i] = sum([shares[ticker] * data[ticker][i] for ticker in column_names])
+    print(data)
+    for i in range(inspection_window, len(data)):
+        long = True
+        pair1, pair2 = column_names[0], column_names[1]
+        if data['signal'][i] == 1 and data['signal'][i-1] != 1:
+            #enter long position
+            long = True
+            data.at[data.index[i], 'long'] = data['capital'][i-1] / data[pair1][i]
+            data.at[data.index[i], 'short']= (data['capital'][i-1]*ratio) /data[pair2][i]
+            data.at[data.index[i], 'cash'] = data['short'][i] * data[pair2][i]
+            data.at[data.index[i], 'capital'] = data['long'][i] * data[pair1][i] - data['short'][i] * data[pair2][i] + data['cash'][i]
+            print(data['capital'][i])
 
-    #     if data['signal'][i] == -1 and data['signal'][i-1] == 0:
-    #         #enter short position
-    #     if data['signal'][i] == 0:
-    #         #exit position
+        elif data['signal'][i] == -1 and data['signal'][i-1] != -1:
+            #enter short position
+            long = False
+            data.at[data.index[i], 'short'] = (data['capital'][i-1] *(1/ratio)) / data[pair1][i]
+            data.at[data.index[i], 'long']= (data['capital'][i-1]) /data[pair2][i]
+            data.at[data.index[i], 'cash'] = data['short'][i] * data[pair1][i]
+            data.at[data.index[i], 'capital'] = data['long'][i-1] * data[pair2][i] - data['short'][i-1] * data[pair1][i] + data['cash'][i-1]
 
-    data['capital'] = 1
+        elif data['signal'][i] == 0:
+            #exit position
+            if data['signal'][i-1] > 0:
+                data.at[data.index[i], 'capital'] = data['long'][i-1] * data[pair1][i] - data['short'][i-1] * data[pair2][i] + data['cash'][i-1]
+            elif data['signal'][i-1] < 0:
+                data.at[data.index[i], 'capital'] = data['long'][i-1] * data[pair2][i] - data['short'][i-1] * data[pair1][i] + data['cash'][i-1]
+            else:
+                data['capital'][i] = data['capital'][i-1]
+            data.at[data.index[i], 'long'] = 0
+            data.at[data.index[i], 'short'] = 0
+            data.at[data.index[i], 'cash'] = data['capital'][i]
+            
+        else:
+            #hold position
+            data.at[data.index[i], 'long'] = data['long'][i-1]
+            data.at[data.index[i], 'short'] = data['short'][i-1]
+            data.at[data.index[i], 'cash'] = data['cash'][i-1]
+            if data['signal'][i] > 0:
+                data.at[data.index[i], 'capital'] = data['long'][i] * data[pair1][i] - data['short'][i] * data[pair2][i] + data['cash'][i]
+            elif data['signal'][i] < 0:
+                data.at[data.index[i], 'capital'] = data['long'][i] * data[pair2][i] - data['short'][i] * data[pair1][i] + data['cash'][i]
     return data
 
 def buy_and_hold(data:pd.DataFrame, initial_capital:str):
@@ -93,61 +129,61 @@ def momentum(data:pd.DataFrame, initial_capital:str):
     pass
 
 def reversion_signal(residual, threshold=0.025):
-    if residual == 0:
+    if 0.01 > residual > -0.01:
         return 0
     elif residual > threshold:
         return 1
     elif residual < -threshold:
         return -1
-    else:
-        return 0.001
+    elif 0.01 <= residual <= threshold:
+        return 0.01
+    elif -0.01 >= residual >= -threshold:
+        return -0.01
 
 if __name__ == "__main__":
     start_date = datetime.datetime(2013,1,1)
     end_date = datetime.datetime(2024,6,1)
     # slope = 0.9131566687899764 
     # intercept = 0.22382541828609814
-    AUD = download_data('AUD=X', start_date, end_date)
-    NZD = download_data('NZD=X', start_date, end_date)
-    print(np.shape(AUD), np.shape(NZD))
-    residues = NZD - slope * AUD - intercept 
-    plt.plot(residues)
-    plt.show()
+    # AUD = download_data('AUD=X', start_date, end_date)
+    # NZD = download_data('NZD=X', start_date, end_date)
+    # print(np.shape(AUD), np.shape(NZD))
+    # residues = NZD - slope * AUD - intercept 
+    # plt.plot(residues)
+    # plt.show()
 
     cur_list1 = ['AUD=X', 'NZD=X']
     cur_list2 = ['SGD=X', 'EUR=X']
-    df, capital_over_time, sharpe_ratio, max_drawdown = backtest(datetime.datetime(2013,1,1), datetime.datetime(2024,6,1), cur_list2, inversion_strategy)
-    plt.plot(df['residual'])
-    #plt.plot(df['signal'])
-    plt.show()
+    df, capital_over_time, sharpe_ratio, max_drawdown = backtest(datetime.datetime(2013,1,1), datetime.datetime(2024,6,1), cur_list1, inversion_strategy)
+    df.to_csv('mean_reversion.csv')
 
-    # print(f'sharpe ratio: {sharpe_ratio}')
-    # print(f'max drawdown: {max_drawdown}')
+    print(f'sharpe ratio: {sharpe_ratio}')
+    print(f'max drawdown: {max_drawdown}')
 
-    # fig, axs = plt.subplots(2, 1, figsize=(12, 10))
+    fig, axs = plt.subplots(2, 1, figsize=(12, 10))
 
-    # currency_columns = df.columns[df.columns.str.contains('=')]  # Filter currency columns
-    # for column in currency_columns:
-    #     axs[0].plot(df.index, df[column], label=column)
+    currency_columns = df.columns[df.columns.str.contains('=')]  # Filter currency columns
+    for column in currency_columns:
+        axs[0].plot(df.index, df[column], label=column)
     
-    # axs[0].set_title('Currency Exchange Rates')
-    # axs[0].set_xlabel('Date')
-    # axs[0].set_ylabel('Exchange Rate')
-    # axs[0].legend()
-    # axs[0].grid()
+    axs[0].set_title('Currency Exchange Rates')
+    axs[0].set_xlabel('Date')
+    axs[0].set_ylabel('Exchange Rate')
+    axs[0].legend()
+    axs[0].grid()
 
-    # # Plot capital
-    # axs[1].plot(df.index, df['capital'], color='green', label='Capital')
-    # axs[1].set_title('Capital Over Time')
-    # axs[1].set_xlabel('Date')
-    # axs[1].set_ylabel('Capital')
-    # axs[1].legend()
-    # axs[1].grid()
+    # Plot capital
+    axs[1].plot(df.index, df['capital'], color='green', label='Capital')
+    axs[1].set_title('Capital Over Time')
+    axs[1].set_xlabel('Date')
+    axs[1].set_ylabel('Capital')
+    axs[1].legend()
+    axs[1].grid()
 
-    # output_dir = 'plots'
-    # os.makedirs(output_dir, exist_ok=True) 
-    # plt.savefig(os.path.join(output_dir, 'buy_and_hold.png'))
+    output_dir = 'plots'
+    os.makedirs(output_dir, exist_ok=True) 
+    plt.savefig(os.path.join(output_dir, 'mean_reversion.png'))
 
-    # plt.tight_layout()
-    # plt.show()
+    plt.tight_layout()
+    plt.show()
 
